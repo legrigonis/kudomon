c44c4a70b86de2eca0632a58715c8178f0e88b74
legrigonis <grigonism@gmail.com>
2017-04-20 16:36:39 +0100

    Part 2 Test

--- tests/test_part2.rb
+++ tests/test_part2.rb
@@ -12,5 +12,31 @@ class TestPart1 < Test::Unit::TestCase
     assert_equal(Game.eucl_sq_distance([0, 0], [2, 2]), 8)
   end
 
+  def test_catch_kudomon
+    #additional setup
+    kudomon_close= Sourbulb.new([20, 10], 200, 30)
+    @game.add_kudomon(kudomon_close)
+    kudomon_med= Sourbulb.new([40, 45], 200, 30)
+    @game.add_kudomon(kudomon_med)
+    kudomon_far= Sourbulb.new([90, 95], 200, 30)
+    @game.add_kudomon(kudomon_far)
+    trainer = Trainer.new(@game,  "Martin", [21, 11])
+    @game.add_trainer(trainer);
+    #finding closest Kukomon
+    assert_equal(trainer.nearest_kudomon, kudomon_close, "Nearest Kudomon was not found")
+    #catching closest kudomon
+    trainer.catch_kudomon
+    assert(!(@game.kudomons.include? kudomon_close), "Kudomon catching true positive failed")
+    assert((@game.kudomons.include? kudomon_med), "Kudomon catching false positive failed")
+    assert((@game.kudomons.include? kudomon_far), "Kudomon catching false positive failed")
+  end
+
+  def test_not_catch_kudomon
+    trainer = Trainer.new(@game,  "Martin", [21, 11])
+    @game.add_trainer(trainer);
+    assert_equal(trainer.nearest_kudomon, nil)
+    assert_raise(RuntimeError){trainer.catch_kudomon}
+  end
+
 
 end
